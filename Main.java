da07d3446035863cc951461aae78a5dcd713975c
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-05-21 18:24:00 +0100

    ss

---
+++ Main.java
@@ -0,0 +1,74 @@
+
+//it should be in every Java Program 
+public class Main {
+
+    // it should be in every Java Class
+    public static void main(String[] args) {
+
+        // println it's for printing a message or data to the screen
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+
+        /*
+         * print , in the same line
+         * , not like Print In
+         */
+        System.out.println(32142);
+
+        System.out.println(32142 + 2132132);
+
+        System.out.println(2 * 5);
+
+        String name = "Yahya";
+
+        System.out.println(name);
+
+        int myNum = 1500000;
+        System.out.println(myNum);
+
+        myNum = 10;
+
+        System.out.println(myNum);
+
+        final int sad = 20;
+
+        System.out.println(sad);
+
+        System.out.println(name + " Mablanch");
+
+        String firstName = "Black ";
+        String lastName = "White";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        int x = 5, y = 6, z = 50;
+
+        int s, tg, asd;
+        s = tg = asd = 50;
+        System.out.println(s + tg + asd);
+
+        int Num = 5; // Integer (whole number)
+        float myFloatNum = 5.99f; // Floating point number
+        char myLetter = 'D'; // Character
+        boolean myBool = true; // Boolean
+        String myText = "Hello"; // String
+
+        byte myByte = 100;
+        System.out.println(myByte);
+
+        long LongNumber = 15000000000L;
+        System.out.println(LongNumber);
+
+        float myFloat = 5.75f;
+        System.out.println(myFloat);
+
+        double myDouble = 19.99d;
+        System.out.println(myDouble);
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun); // Outputs true
+        System.out.println(isFishTasty); // Outputs false
+    }
+}
